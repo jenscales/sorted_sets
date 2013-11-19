037f2020b9dafdcb7428b9fcacc3de6614cc7526
Jennifer Scales <jen_scales@yahoo.com>
2013-11-19 13:06:57 -0600

    added functioning class BasicArraySortedSet, HashSorted Set. Working on ArraySortedSet

--- sorted_set.rb
+++ sorted_set.rb
@@ -1,6 +1,7 @@
 class BasicArraySortedSet
   # Create your internal data structure here. It should be an empty array
   def initialize
+    @array = []
   end
 
   def name
@@ -9,14 +10,26 @@ class BasicArraySortedSet
 
   # Use the array's native insert method
   def insert(element)
+    if @array.include?(element)
+      return false
+    else
+      @array << element
+      return element
+    end
   end
 
   # Use the array's native include method
   def include?(element)
+    if @array.include? element
+      return true
+    else
+      return false
+    end
   end
 
   # Use the array's native sort method
   def get_sorted_array
+    @array.sort
   end
 end
 
@@ -24,6 +37,7 @@ end
 # the sorted set's methods
 class HashSortedSet
   def initialize
+    @hash = {}
   end
 
   def name
@@ -32,12 +46,24 @@ class HashSortedSet
 
   # Insert the key value pair (element, true)
   def insert(element)
+    if @hash[element] == true
+      return false
+    else
+      @hash[element] = true
+      return element
+    end     
   end
 
   def include?(element)
+    if @hash.has_key?(element)
+      return true
+    else 
+      return false
+    end
   end
 
   def get_sorted_array
+    @hash.keys.sort
   end
 end
 
@@ -46,6 +72,8 @@ end
 # it when asking for sorted array
 class ArraySortedSet
   def initialize
+    @ary = []
+    @sorted_ary = @ary.sort
   end
 
   def name
@@ -54,13 +82,25 @@ class ArraySortedSet
 
   # Insert the element at the proper index to maintain the sort order
   def insert(element)
+    if @sorted_ary.include? element
+      return false
+    else 
+      @sorted_ary << element
+      return element
+    end 
   end
 
   def include?(element)
+    if @sorted_ary.include? element
+      return true
+    else
+      return false
+    end
   end
 
   # You should be able to simply return the original array
   def get_sorted_array
+    @sorted_ary.sort
   end
 end
 
